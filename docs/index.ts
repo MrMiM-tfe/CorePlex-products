92fd2540c3daf673d1ff98e468f6fd2dbc3acca2
MrMiM <mahdikhakbaz7@gmail.com>
2023-05-16 23:16:57 +0330

    add docs

---
+++ docs/index.ts
@@ -0,0 +1,13 @@
+import swaggerJSDoc from "swagger-jsdoc";
+
+const paths:  swaggerJSDoc.Paths = {
+    "/" : {
+        get: {
+
+        }
+    }
+}
+
+export default {
+    paths
+}
--- index.ts
+++ index.ts
@@ -1,1 +1,7 @@
-export { default as routes } from "./routes"
+import routes from "./routes"
+import docs from "./docs";
+
+export default {
+    routes,
+    docs
+}
